8352a4d8a81e489c3f9167115f732d69a80903d6
DavidGTeklea <87445117+DavidGTeklea@users.noreply.github.com>
2025-02-01 12:45:55 -0500

    email/pwd accessible from dashboard now

--- horizon_finance_frontend/src/Dashboard.js
+++ horizon_finance_frontend/src/Dashboard.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Navbar from "../src/components/Navbar";
 import Chart from "../src/components/Chart";
+import { useLocation } from 'react-router-dom';
+
 
 function Dashboard() {
-  return (
-    <div className="dashboard">
-      <Navbar />
-      <div className="content">
-        <h1>Dashboard</h1>
-        <Chart />
-      </div>
-    </div>
-  );
+    const location = useLocation();
+    const { email, password } = location.state || {}; // stored email and passwd
+
+
+    return (
+        <div className="dashboard">
+            <Navbar />
+            <div className="content">
+                <h1>Dashboard</h1>
+                <Chart />
+            </div>
+        </div>
+    );
 }
 
 export default Dashboard;
