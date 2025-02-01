e8fd1cb263746f1670786e4ca46943b5deb1a75b
DavidGTeklea <87445117+DavidGTeklea@users.noreply.github.com>
2025-02-01 15:41:56 -0500

    updating css fixes

--- horizon_finance_frontend/src/App.js
+++ horizon_finance_frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./SignUpForm.js";
-import Dashboard from "./Dashboard.js";
+import Dashboard from "./DashBoard.js";
 import WhatIf from "./WhatIf.js";
 import HomePage from './HomePage';
 
