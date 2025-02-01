3ded7b167d8eea579200f827e59377d102414e0e
Dominick Dellecave <dominick@dellecave.com>
2025-02-01 15:47:23 -0500

    plaid link flow

--- horizon_finance_frontend/src/Dashboard.js
+++ horizon_finance_frontend/src/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../src/components/Navbar";
 import Chart from "../src/components/Chart";
+import ConnectBank from "./services/plaid_service";
 
 function Dashboard() {
   return (
@@ -9,6 +10,9 @@ function Dashboard() {
       <div className="content">
         <h1>Dashboard</h1>
         <Chart />
+      </div>
+      <div>
+        <ConnectBank user_id="user1"/>
       </div>
     </div>
   );
---
+++ horizon_finance_frontend/src/services/plaid_service.js
@@ -0,0 +1,67 @@
+import { usePlaidLink } from 'react-plaid-link';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+axios.defaults.baseURL = "http://127.0.0.1:5000/plaid";
+
+export default function ConnectBank({ user_id }) {
+    const [ linkToken, setLinkToken ] = useState(null);
+    const [ publicToken, setPublicToken ] = useState(null);
+
+
+    useEffect(() => {
+        async function fetch() {
+            console.log("Sending user_id:", user_id);
+            const response = await axios.post("/link_token", 
+                { user_id: user_id }, 
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+            );
+            const linkToken = response.data.link_token
+            setLinkToken(linkToken)
+            
+        }
+        fetch();
+    }, [user_id]);
+
+    const { open, ready } = usePlaidLink(
+        linkToken
+            ? {
+                token: linkToken,
+                onSuccess: async (public_token, metadata) => {
+                    setPublicToken(public_token);
+                    try {
+                        console.log(publicToken)
+                        const response = await axios.post("/access_token", 
+                            { public_token: public_token }, 
+                            {
+                                headers: {
+                                    "Content-Type": "application/json"
+                                }
+                            }
+                        );
+                        console.log(response.data);
+                    } catch (error) {
+                        console.error("Error exchanging public token:", error);
+                    }
+                },
+                onExit: (error, metadata) => {
+                    console.log("Exited link before establishing connection");
+                }
+            }
+            : {} // Prevent errors if linkToken is null
+    );
+
+
+    return (
+        <button onClick={() => open()} disabled={!ready}>
+            Connect a bank account
+        </button>
+    );
+
+}
+
+
